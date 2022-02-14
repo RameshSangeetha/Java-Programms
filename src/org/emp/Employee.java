0ebc78f058ab170e917f09d9818c5c4765c01cfc
RameshSangeetha <b.sc.ramesh1990@gmail.com>
2022-02-14 22:18:50 +0530

    Sample Java programmes

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,76 @@
+package org.emp;
+
+public class Employee {
+
+public void empid() {
+		int a = 7024;
+		String b = "Ramesh S";
+		String c = "11-11-1990";
+		String d = "9094776108";
+		String e = "b.sc.ramesh1990@gmail.com";
+		String f = "1/22, venkateswara nagar, Ramapurm Chennai-89";
+		System.out.print("empId:"+" "+a+"\n"+"empname:"+" "+(b)+"\n"+"empDob:"+" "+(c)+"\n"+"empPhone:"+" "+(d)+"\n"+"empEmail:"+" "+(e)+"\n"+"empAddress:"+" "+(f));
+		
+	}
+//	public static void main(String[] args) {
+	//	Employee obj = new Employee();
+		//obj.empid();
+			
+	
+	
+	public void greensOmr() {
+
+		String a = "No. 19, Balamurugan Garden, OMR Road,\n"
+				+ "Thoraipakkam, Kanchipuram District.";
+			System.out.println("Greens OMR Address:"+"\n"+a);
+		
+	}
+	
+	public void greensThambaram() {
+		String b = "No.1, Appa Rao Colony,\nTambaram,\nSanatorioum,\nChennai-600 047.";
+System.out.println("Greens Tamaram Address:"+"\n"+b);
+		
+	}
+	
+		public void greensPorur() {
+		String c = "No.149, 1st floor opposite to DLF IT Park,\nRamapuram,\nAbove Axis Bank,\nPorur,\nChennai-89." ;
+	System.out.println("Greens Porur Address:"+"\n"+c);	
+	}
+	
+/*	 public static void main(String[] args) {
+			Employee obj = new Employee();
+			obj.greensOmr();
+			obj.greensThambaram();
+			obj.greensPorur();
+		 
+	}
+*/	
+	public void tamilLanguage() {
+		int a = 50;
+		System.out.println("Tamil Language Marks:"+" "+a);
+	}
+	public void englishLanguage() {
+		int b = 80;
+		System.out.println("English Language Marks:"+" "+b);
+			}
+	public void hindiLanguage() {
+		int c = 70;
+		System.out.println("Hindi Language Marks:"+" "+c);
+	}
+	 /*public static void main(String[] args) {
+			Employee obj = new Employee();
+			obj.tamilLanguage();
+			obj.englishLanguage();
+			obj.hindiLanguage();
+		
+		
+	}	*/
+	public void sourthindia() {
+
+		String c = "Coutry";
+		System.out.println(c);
+		
+	}
+}
+
+
---
+++ src/org/emp/StateDetails.java
@@ -0,0 +1,19 @@
+package org.emp;
+
+public class StateDetails extends Employee {
+	
+	public static void main(String[] args) {
+		Employee obj = new Employee();
+		obj.tamilLanguage();
+		obj.englishLanguage();
+		obj.hindiLanguage();
+		obj.sourthindia();
+		obj.empid();
+		obj.greensOmr();
+		obj.greensThambaram();
+		obj.greensPorur();
+		obj.tamilLanguage();
+		obj.englishLanguage();
+		obj.hindiLanguage();
+}
+}
